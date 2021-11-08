6cb8e240b5c6bc7270df3682f79920c0953a6e50
Joacof17 <joaco.fernandez.2212@gmail.com>
2021-11-08 12:37:09 -0300

    both activities working

--- app/src/main/java/com/example/myapp2/MainActivity.java
+++ app/src/main/java/com/example/myapp2/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.myapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button ir;
+    public EditText et;
+    public TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ir = (Button) findViewById(R.id.buttonIr);
+
+        ir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ir = new Intent(MainActivity.this, act2.class);
+                startActivity(ir);
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/myapp2/act2.java
+++ app/src/main/java/com/example/myapp2/act2.java
@@ -3,12 +3,50 @@ package com.example.myapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class act2 extends AppCompatActivity {
+
+    public Button mButtonSelecionarBtn;
+    public TextView mTextViewFechaSeleccionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act2);
+
+        mButtonSelecionarBtn = findViewById(R.id.Seleccionar);
+        mTextViewFechaSeleccionada = findViewById(R.id.fechaSeleccionada);
+
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Selecciona una fecha");
+        final MaterialDatePicker materialDatePicker = builder.build();
+
+        mButtonSelecionarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(), materialDatePicker.getTag());
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                mTextViewFechaSeleccionada.setText("Se eligió " + materialDatePicker.getHeaderText());
+            }
+        });
+
+
     }
+
+
 }
