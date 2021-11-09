ddcd21271d6ab06cb04ffc1ae02a8a6c8d571f56
Joacof17 <joaco.fernandez.2212@gmail.com>
2021-11-09 09:18:32 -0300

    notification almost ready

--- app/src/main/java/com/example/myapp2/act2.java
+++ app/src/main/java/com/example/myapp2/act2.java
@@ -4,10 +4,15 @@ import static com.google.android.material.timepicker.MaterialTimePicker.INPUT_MO
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
@@ -18,14 +23,20 @@ import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class act2 extends AppCompatActivity {
 
     public Button mButtonSeleccionarFechaBtn;
     public Button mButtonSeleccionarHoraBtn;
+    public Button mGuardarDatos;
     public TextView mTextViewFechaSeleccionada;
     public TextView mTextViewHoraSeleccionada;
+    private int hora;
+    private int min;
+    private int seg;
+    private Date fecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +47,11 @@ public class act2 extends AppCompatActivity {
         mButtonSeleccionarHoraBtn = findViewById(R.id.SeleccionarHora);
         mTextViewFechaSeleccionada = findViewById(R.id.fechaSeleccionada);
         mTextViewHoraSeleccionada = findViewById(R.id.horaaSeleccionada);
-
+        mGuardarDatos = findViewById(R.id.buttonGuardar);
 
         MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
         builder.setTitleText("Selecciona una fecha");
-        final MaterialDatePicker materialDatePicker = builder.build();
+        final MaterialDatePicker<Long> materialDatePicker = builder.build();
 
         mButtonSeleccionarFechaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,10 +60,16 @@ public class act2 extends AppCompatActivity {
             }
         });
 
-        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
             @Override
-            public void onPositiveButtonClick(Object selection) {
-                mTextViewFechaSeleccionada.setText("Se eligió " + materialDatePicker.getHeaderText());
+            public void onPositiveButtonClick(Long selection) {
+                TimeZone zona = TimeZone.getDefault();
+                int difHoraria = zona.getOffset(new Date().getTime()) * -3;
+                SimpleDateFormat sf = new SimpleDateFormat("dd-MM-yyyy");
+                Date date = new Date(selection + difHoraria);
+                fecha = date;
+                mTextViewFechaSeleccionada.setText("Se eligió " + sf.format(date));
+
             }
         });
 
@@ -76,9 +93,35 @@ public class act2 extends AppCompatActivity {
 
         materialTimePicker.addOnPositiveButtonClickListener(dialog -> {
             mTextViewHoraSeleccionada.setText("Se eligió "+materialTimePicker.getHour() + ":" + materialTimePicker.getMinute());
+            hora = materialTimePicker.getHour()*60*60*1000;
+            min = materialTimePicker.getMinute()*60*1000;
+
+
+        });
+
+        mGuardarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(fecha);
+
+                // Recuperando el alarm manager
+                final AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                // Seteo de la alarma
+                Long tiempoEnMillis = calendar.getTimeInMillis() + hora + min;
+                Intent intent = new Intent(act2.this, RecordatorioReceiver.class);
+                PendingIntent pendingIntentConActionParaMiBroadcastReceiver = PendingIntent.getBroadcast(act2.this, 1, intent, 0);
+                alarm.set(AlarmManager.RTC_WAKEUP, tiempoEnMillis, pendingIntentConActionParaMiBroadcastReceiver);
+
+                Toast msj = Toast.makeText(getApplicationContext(), "Funca", Toast.LENGTH_SHORT);
+                msj.show();
+            }
         });
 
     }
+
+
 
 
 }
