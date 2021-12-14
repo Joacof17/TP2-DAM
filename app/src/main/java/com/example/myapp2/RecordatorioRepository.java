f295637e1a6f7d854bfe089d91635ff8b4467d95
Martin Escowich <escowichmartin@gmail.com>
2021-12-13 22:00:51 -0300

    Falto subir el repository

---
+++ app/src/main/java/com/example/myapp2/RecordatorioRepository.java
@@ -0,0 +1,9 @@
+package com.example.myapp2;
+
+public class RecordatorioRepository {
+        private final RecordatorioDataSource datasource;
+        public RecordatorioRepository(final RecordatorioDataSource datasource) {
+            this.datasource = datasource;
+        }
+
+}
